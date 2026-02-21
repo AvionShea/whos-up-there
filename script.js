4cfcf0fcab6fa4f3d2cb3bfe90bc26440608f36e
Avion C. <83898002+AvionShea@users.noreply.github.com>
2026-02-21 15:24:15 -0500

    troubleshoot: Update astronauts API URL to use new proxy

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ async function issLocation() {
 
 // Astronauts currently in space
 async function peopleInSpace() {
-    const peopleInSpaceUrl = "https://corsproxy.io/?https://api.open-notify.org/astros.json";
+    const peopleInSpaceUrl = "https://thingproxy.freeboard.io/fetch/http://api.open-notify.org/astros.json";
     try {
         const peopleInSpaceResponse = await fetch(peopleInSpaceUrl);
         if (!peopleInSpaceResponse.ok) {
